5e27ec2ac3bef8a426949b55fca0253d18ecb7fe
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-03-22 15:38:05 +0100

    css, js and html

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-let questions = [['first question', 'first answer'],['second question', 'second answer'],['third question','third answer'],['fourth question','fourth answer'],['fifth questions','fifth answer']]
-
-
-// Java Script Object Notation
-console.log(JSON.stringify(questions))
-
-
+let data = {
+    questions: [['first question', 'first answer'],['second question', 'second answer'],['third question','third answer'],['fourth question','fourth answer'],['fifth questions','fifth answer']]
+}
+// Getting our localStorage on!
+// let getData = JSON.parse(localStorage.getItem('storeObj'))
+// data = getData
+// Here we create a function that randomly shows the questions on the flashcard with the right answers after.
 let card = document.getElementById('flashcard')
 let flip = 0
 let randomCard = 0
 function flipCard() {
     if(flip === 0) {
         flip = 1
-        card.innerHTML = questions[randomCard][flip]
-        randomCard = Math.floor(Math.random()*questions.length)
+        card.innerHTML = data.questions[randomCard][flip]
+        randomCard = Math.floor(Math.random()*data.questions.length)
     } else {
         flip = 0
-        card.innerHTML = questions[randomCard][flip]
+        card.innerHTML = data.questions[randomCard][flip]
     }
 }
+//this function creates our input data!
 function input() {
     const question = document.getElementById('question')
     const answer = document.getElementById('answer')
     const questionInput = question.value
     const answerInput = answer.value
-    console.log(questionInput, answerInput)
-    questions.push([questionInput, answerInput])
-    console.log(questions)
+    data.questions.push([questionInput, answerInput])
+    question.value = null
+    answer.value = null
+    storeLocal()
 }
+//this is our local storage function!
+function storeLocal() {
+    localStorage.setItem('storeObj', JSON.stringify(data))
+    let getData = JSON.parse(localStorage.getItem('storeObj'))
+    data = getData
+}
+
